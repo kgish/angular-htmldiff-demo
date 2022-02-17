7f3dbebbcbb7352f8235c675504b648d95ae8740
Kiffin Gish <kiffin.gish@planet.nl>
2022-02-17 12:34:28 +0100

    Renderer2 and toolbar.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatTabsModule
+    MatTabsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [ AppComponent ]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
+import { MatTabChangeEvent } from '@angular/material/tabs';
+
 import diff from 'htmldiff-angular';
 
 import { htmlCurrent, htmlPrevious } from './home.data';
 
+type TAG_NAME = 'del' | 'ins';
+type COLOR_HEX = string;
+
+const CLASS_NAME = 'htmldiff';
+const backgroundColor: { [key in TAG_NAME]: COLOR_HEX } = {
+  'del': '#fde8e9',
+  'ins': '#ddffdd'
+}
 
 @Component({
   selector: 'app-home',
@@ -15,12 +25,27 @@ export class HomeComponent implements OnInit {
   htmlPrevious = '';
   htmlDifferences = '';
 
-  constructor() {
+  constructor(private renderer: Renderer2) {
   }
 
   ngOnInit(): void {
     this.htmlCurrent = htmlCurrent;
     this.htmlPrevious = htmlPrevious;
-    this.htmlDifferences = diff(this.htmlPrevious, this.htmlCurrent, 'my-class');
+    this.htmlDifferences = diff(this.htmlPrevious, this.htmlCurrent, CLASS_NAME);
+  }
+
+  selectedTabChange(event: MatTabChangeEvent) {
+    if (event.index === 2) {
+      this.colorizeHtmlDiffs();
+    }
+  }
+
+  colorizeHtmlDiffs() {
+    let key: TAG_NAME;
+    for (key in backgroundColor) {
+      document.querySelectorAll(key + '.' + CLASS_NAME).forEach(
+        el => this.renderer.setStyle(el, 'backgroundColor', backgroundColor[key])
+      );
+    }
   }
 }
